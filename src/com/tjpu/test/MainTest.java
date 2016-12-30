81f78430d5ac6bbb712c377e095c1d0e7663126d
soft1302 <1206978669@qq.com>
2016-12-30 12:07:59 +0800

    update

--- src/com/tjpu/test/MainTest.java
+++ src/com/tjpu/test/MainTest.java
@@ -1,22 +1,35 @@
 package com.tjpu.test;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.tjpu.dao.SysuserMapper;
+import com.tjpu.pojo.Sysuser;
+import com.tjpu.pojo.SysuserExample;
 import com.tjpu.util.JSONUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 整合
 @ContextConfiguration(locations = "classpath:config/spring-mybatis.xml") // 加载配置
 public class MainTest {
-	@Autowired
+	@Resource
 	private SysuserMapper sysuserMapper;
 
 	@Test
 	public void test() {
-		System.out.println(JSONUtils.objectToJsonString(sysuserMapper.selectByPrimaryKey(1)));
+		SysuserExample sysuserExample = new SysuserExample();
+		SysuserExample.Criteria criteria = sysuserExample.createCriteria();
+		criteria.andIdEqualTo(4);
+		SysuserExample.Criteria criteria2 = sysuserExample.createCriteria();
+		criteria2.andIdBetween(1, 3);
+		sysuserExample.or(criteria2);
+		List<Sysuser> lSysusers = sysuserMapper.selectByExample(sysuserExample);
+		System.out.println(JSONUtils.objectToJsonString(lSysusers));
 	}
 }
